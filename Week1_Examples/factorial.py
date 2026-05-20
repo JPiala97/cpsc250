c6c3d39f25ee121912695ed8f6e29f1b879bd3fa
Edward Brash <brash@jlab.org>
2026-05-20 08:33:35 -0400

    Updates

--- Week1_Examples/factorial.py
+++ Week1_Examples/factorial.py
@@ -1,7 +1,13 @@
-def factorial(n):
+def factorial_recursive(n):
     if n == 0 or n == 1:
         return 1
-    return n*factorial(n-1)
+    return n*factorial_recursive(n-1)
 
+def factorial_iterative(n):
+    result = 1
+    for i in range(1, n+1):
+        result *= i
+    return result
 
-print(factorial(275))
+print(factorial_recursive(231))
+print(factorial_iterative(231))
